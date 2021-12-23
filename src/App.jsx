95a0fa9d604dca400991566bb2015bea836dab2d
Anton <anton-babkin@mail.ru>
2021-12-23 13:46:06 +0300

    separateReducer

--- src/App.jsx
+++ src/App.jsx
@@ -14,91 +14,92 @@ import { FormArticle } from './components/FormArticle/FormArticle';
 import { getArticles, loadingIndicator } from './Actions/actionArticles';
 import { getUser } from './Actions/actionUser';
 import { setCurrentPage } from './Actions/actionPages';
+import * as selectors from './Selectors/Selectors';
 
 export const App = () => {
-  const { count, loading } = useSelector((state) => state.reducerArticle);
-  const { user, newUser } = useSelector((state) => state.reducerUser);
-  const { currentPage } = useSelector((state) => state.reducerPages);
+	const { count, loading } = useSelector(selectors.articles);
+	const { user, newUser } = useSelector(selectors.user);
+	const { currentPage } = useSelector(selectors.pages);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const { token } = user;
-  useEffect(() => {
-    dispatch(getArticles(0, token));
-    dispatch(loadingIndicator());
-  }, [dispatch, token]);
+	const { token } = user;
+	useEffect(() => {
+		dispatch(getArticles(0, token));
+		dispatch(loadingIndicator());
+	}, [dispatch, token]);
 
-  const paginate = (pageNumber) => {
-    dispatch(getArticles((pageNumber - 1) * 5, token));
-    dispatch(setCurrentPage(pageNumber));
-  };
+	const paginate = (pageNumber) => {
+		dispatch(getArticles((pageNumber - 1) * 5, token));
+		dispatch(setCurrentPage(pageNumber));
+	};
 
-  useEffect(() => {
-    const parseUser = JSON.parse(localStorage.getItem('user'));
-    dispatch(getUser(parseUser));
-  }, [dispatch]);
+	useEffect(() => {
+		const parseUser = JSON.parse(localStorage.getItem('user'));
+		dispatch(getUser(parseUser));
+	}, [dispatch]);
 
-  useEffect(() => {
-    localStorage.setItem('user', JSON.stringify(user));
-  }, [dispatch, user]);
+	useEffect(() => {
+		localStorage.setItem('user', JSON.stringify(user));
+	}, [dispatch, user]);
 
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="main">
-          <Route path="/" component={Article} exact />
-          <Route path="/articles" component={Article} exact />
-          <Route path="/sign-in" component={SignIn} />
-          <Route path="/sign-up" component={SignUp} />
-          <Route path="/profile" component={EditProfile} />
-          <Route path="/new-article" component={FormArticle} />
-          <Route path="/articles/:slug/edit" component={FormArticle} />
-          <Route
-            path="/articles/:slug"
-            exact
-            render={({ match }) => {
-              const { slug } = match.params;
-              return <ArticlePage itemSlug={slug} />;
-            }}
-          />
-          {user.user && <Redirect to="/articles" />}
-          {newUser.user && <Redirect to="/sign-in" />}
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+				<div className="main">
+					<Route path="/" component={Article} exact />
+					<Route path="/articles" component={Article} exact />
+					<Route path="/sign-in" component={SignIn} />
+					<Route path="/sign-up" component={SignUp} />
+					<Route path="/profile" component={EditProfile} />
+					<Route path="/new-article" component={FormArticle} />
+					<Route path="/articles/:slug/edit" component={FormArticle} />
+					<Route
+						path="/articles/:slug"
+						exact
+						render={({ match }) => {
+							const { slug } = match.params;
+							return <ArticlePage itemSlug={slug} />;
+						}}
+					/>
+					{user.user && <Redirect to="/articles" />}
+					{newUser.user && <Redirect to="/sign-in" />}
 
-          <div className="pagination">
-            <Route
-              path="/articles"
-              exact
-              render={() =>
-                !loading && (
-                  <Pagination
-                    size="small"
-                    total={count}
-                    showSizeChanger={false}
-                    onChange={paginate}
-                    current={currentPage}
-                  />
-                )
-              }
-            />
-            <Route
-              path="/"
-              exact
-              render={() =>
-                !loading && (
-                  <Pagination
-                    size="small"
-                    total={count}
-                    showSizeChanger={false}
-                    onChange={paginate}
-                    current={currentPage}
-                  />
-                )
-              }
-            />
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+					<div className="pagination">
+						<Route
+							path="/articles"
+							exact
+							render={() =>
+								!loading && (
+									<Pagination
+										size="small"
+										total={count}
+										showSizeChanger={false}
+										onChange={paginate}
+										current={currentPage}
+									/>
+								)
+							}
+						/>
+						<Route
+							path="/"
+							exact
+							render={() =>
+								!loading && (
+									<Pagination
+										size="small"
+										total={count}
+										showSizeChanger={false}
+										onChange={paginate}
+										current={currentPage}
+									/>
+								)
+							}
+						/>
+					</div>
+				</div>
+			</div>
+		</Router>
+	);
 };
