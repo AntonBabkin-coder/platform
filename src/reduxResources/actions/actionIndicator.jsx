f7dbbcc75bbbab6d6e9fe362cb647753225dbb84
Anton <anton-babkin@mail.ru>
2021-12-24 16:06:22 +0300

    refact

--- src/reduxResources/actions/actionIndicator.jsx
+++ src/reduxResources/actions/actionIndicator.jsx
@@ -1,5 +1,2 @@
 export const showModalPage = () => ({ type: 'SHOW_MODAL' });
 export const hideModalPage = () => ({ type: 'HIDE_MODAL' });
-
-// export const HIDE_MODAL = 'HIDE_MODAL';
-// export const SHOW_MODAL = 'SHOW_MODAL';
